a0ef2a03b988914d2c196a1eace828642e4e3fc3
wang qiang <wqpod2g@gmail.com>
2015-01-03 22:48:17 +0800

    1.4

--- src/iip/util/OurConnection.java
+++ src/iip/util/OurConnection.java
@@ -1,11 +1,3 @@
-/**
-* Copyright (c) 2012-20XX XX Ltd. All Rights Reserved. 
-* 
-* This software is the confidential and proprietary information of 
-* XX. You shall not disclose such Confidential Information 
-* and shall use it only in accordance with the terms of the agreements 
-* you entered into with XX. 
-**/ 
 package iip.util;
 
 import java.sql.Connection;
@@ -15,13 +7,13 @@ import java.sql.DriverManager;
  * This class is used for 
  * 
  * @version 1.0, 2012-3-20 
- * @author  xu  dongdong
+ * @author  wangqiang
  **/
 
 public class OurConnection {
 	private static Connection conn = null;
 	private static final String DBDRIVER = "com.mysql.jdbc.Driver" ;
- 	private static final String DBURL = "jdbc:mysql://127.0.0.1:3306/wqtest" ;
+ 	private static final String DBURL = "jdbc:mysql://114.212.83.18:3306/wqtest" ;
 	private static final String DBUSER = "root" ;
 	private static final String DBPASSWORD = "1234" ;
 
